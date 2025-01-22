4db09683dd32236e11b5740ffe2b720b422baae5
UltraDeveloper7 <konstantinostoutounas7@outlook.com.gr>
2025-01-22 03:55:54 +0200

    Fixing the color of the main window

--- src/core/App.cpp
+++ src/core/App.cpp
@@ -44,7 +44,7 @@ void App::OnUpdate()
 
 	HandleState();
 
-	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	if (world_)
